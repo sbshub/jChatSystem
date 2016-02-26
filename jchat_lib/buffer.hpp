fbf99f3e8e25f285423697e85ae8bfc1a5e2dbce
Eyaz Rehman <scrappycocos@hotmail.com>
2016-02-26 12:40:13 -0500

    Added comments and instructions for the Buffer class, however first complete the rest of the project and then document the source code.

--- jchat_lib/buffer.hpp
+++ jchat_lib/buffer.hpp
@@ -16,13 +16,44 @@
 #include <stdlib.h>
 
 namespace jchat {
+// Provides a way to write to a buffer with objects of any type or to serialize
+// an object type to a byte array
+// Example:
+//    struct Person {
+//      char FirstName[32];
+//      char LastName[32];
+//      int Age;
+//    }
+//    ...
+//    Person p;
+//    strcpy(p.FirstName, "John");
+//    strcpy(p.LastName, "Doe");
+//    p.Age = 25;
+//
+//    Buffer b;
+//    b.Write(p);
+//    b.Rewind();
+//
+//    Person p2;
+//    b.Read(&p2);
+//    printf("%s %s is %i years old.\n", p2.FirstName, p2.LastName, p2.Age);
 class Buffer {
+  // Internal buffer used for storing the data written. Used for later reading
+  // or writing.
   std::vector<uint8_t> buffer_;
+
+  // The currrent position of the buffer. Used for reading and writing to
+  // determine if data needs to be appended or overwritten.
   size_t current_position_;
+
+  // Used to flip endian order if required. In case two different hosts with
+  // different endian orders have accessed/written to the buffer. For example,
+  // AMD vs Intel CPUs or Network vs Host endian order.
   bool flip_endian_;
 
   template<typename _TData>
   static void FlipEndian(_TData *buffer, size_t size) {
+    // Reverse the array
     uint8_t *p_buffer = *(uint8_t **)&buffer;
     for (size_t i = 0; i < size / 2; i++) {
       uint8_t tmp = p_buffer[i];
@@ -38,26 +69,34 @@ public:
 
   Buffer(const uint8_t *buffer, size_t size, bool flip_endian = false)
     : current_position_(0), flip_endian_(flip_endian) {
-      for (size_t i = 0; i < size; i++) {
-        buffer_.push_back(buffer[i]);
-      }
+    // Copy the data to the internal buffer
+    for (size_t i = 0; i < size; i++) {
+      buffer_.push_back(buffer[i]);
+    }
   }
 
   ~Buffer() {
-    Clear();
+    // Set all the data to 0 in case we had important data in the buffer
+    for (size_t i = 0; i < buffer_.size(); i++) {
+      buffer_[i] = 0;
+    }
   }
 
   template<typename _TData>
   bool Read(_TData *obj) {
+    // Check if there is enough data to read
     size_t size = sizeof(_TData);
     if (current_position_ + size > buffer_.size()) {
       return false;
     }
+    // Read the data into the object buffer
     uint8_t *p_buffer = *(uint8_t **)&obj;
     for (size_t i = 0; i < size; i++) {
       p_buffer[i] = buffer_.data()[current_position_];
       current_position_++;
     }
+    // Flip the endian order of the object
+    // if needed
     if (flip_endian_) {
       FlipEndian(obj, size);
     }
@@ -66,9 +105,11 @@ public:
 
   template<typename _TData>
   bool ReadArray(_TData *obj, size_t size) {
+    // Check if there is any data to read
     if (current_position_ + size * sizeof(_TData) - 1 > buffer_.size()) {
       return false;
     }
+    // Read the array object by object
     for (size_t i = 0; i < size; i++) {
       if (!Read<_TData>(&obj[i])) {
         return false;
@@ -79,12 +120,16 @@ public:
 
   template<typename _TData>
   void Write(_TData obj) {
+    // Get the pointer to the data object and flip the object in
+    // case the endian order needs changing
     uint8_t *buffer = (uint8_t *)&obj;
     size_t size = sizeof(_TData);
     if (flip_endian_) {
       FlipEndian(buffer, size);
     }
     for (size_t i = 0; i < size; i++) {
+      // Check if we need to append to the buffer or
+      // replace existing data
       if (current_position_ + i >= this->buffer_.size()) {
         buffer_.push_back(buffer[i]);
       } else {
@@ -96,6 +141,7 @@ public:
 
   template<typename _TData>
 	void WriteArray(_TData *obj, size_t size) {
+    // Write each object in the array
     for (size_t i = 0; i < size; i++) {
       Write(obj[i]);
     }
@@ -106,6 +152,8 @@ public:
   }
 
   bool SetPosition(size_t current_position) {
+    // If the specified position is past the end of the buffer
+    // return false
     if (current_position > buffer_.size() - 1) {
       return false;
     }
@@ -130,6 +178,8 @@ public:
     for (size_t i = 0; i < buffer_.size(); i++) {
       buffer_[i] = 0;
     }
+
+    // Clear the buffer
     buffer_.clear();
     buffer_.shrink_to_fit();
     current_position_ = 0;
--- jchat_server/src/main.cpp
+++ jchat_server/src/main.cpp
@@ -8,28 +8,11 @@
 */
 
 // Required libraries
-#include "buffer.hpp"
 #include <iostream>
-
-// Required namespaces
-using namespace jchat;
 
 // Program entrypoint
 int main(int argc, char **argv) {
   std::cout << "jChatSystem - Server" << std::endl;
-
-  // Test Buffer class
-  Buffer buffer;
-  buffer.Write<uint64_t>(5666);
-
-  buffer.Rewind();
-
-  uint64_t number = 0;
-  if (buffer.Read(&number)) {
-    std::cout << "I read the number " << number << "!" << std::endl;
-  }
-
-  std::cout << "Buffer size = " << buffer.GetSize() << " bytes" << std::endl;
 
   return 0;
 }
